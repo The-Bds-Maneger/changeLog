055aa119baaf904e4dd6fe98d497cb76fbbc40dc
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-11-16 09:48:28 -0300

    update file

--- src/indexPages.ts
+++ src/indexPages.ts
@@ -1,6 +1,5 @@
 import { httpRequest } from "@the-bds-maneger/core-utils";
 import { JSDOM } from "jsdom";
-
 export const headers = {
   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
   "Accept-Language": "en-US,en;q=0.5",
@@ -14,28 +13,44 @@ export const headers = {
   "Cache-Control": "no-cache"
 };
 
-export async function find() {
-  let page = 0;
-  const dataLog = [];
+export async function listArticles() {
+  const baseUrl = "https://feedback.minecraft.net";
+  let nextPage = baseUrl+"/hc/en-us/sections/360001186971?page=0";
+  const dataLog: {link: string, title: string}[] = [];
   while (true) {
-    const data = await httpRequest.bufferFetch({
-      url: `https://feedback.minecraft.net/hc/en-us/sections/360001186971?page=${page}`,
-      headers
-    }).then(({data}) => data.toString("utf8"));
-    page++
-    //console.log(data);
-    const dom = new JSDOM(data);
-    const dd = Array.from(dom.window.document.querySelectorAll("ul[class=\"article-list\"] > li")||[]);
-    dd.forEach(li => {
-      let link = li.querySelector("a").href;
-      if (link.startsWith("/")) link = "https://feedback.minecraft.net"+link;
+    if (!nextPage) break;
+    const {document} = (new JSDOM(await httpRequest.bufferFetch({url: nextPage, headers}).then(({data}) => data.toString("utf8")))).window;
+    for (const li of Array.from(document.querySelectorAll("ul[class=\"article-list\"] > li")||[])) {
+      let link = li.querySelector("a");
+      if (!link) continue;
       dataLog.push({
-        link,
-        body: li.querySelector("a").innerHTML,
+        link: link.href.startsWith("/")?baseUrl+link.href:link.href,
+        title: link.innerHTML,
       });
-    });
-    if (page >= 5) break;
+    }
+    const nextPageQuery = document.querySelector("li[class=\"pagination-next\"] > a")?.href;
+    if (!nextPageQuery) nextPage = undefined
+    else {
+      if (nextPageQuery.startsWith("/")) nextPageQuery = baseUrl+nextPageQuery;
+      nextPage = nextPageQuery;
+    }
   }
   return dataLog;
 }
-find().then(console.log);
+
+export async function listContent() {
+  const data = await listArticles();
+  const pages: {title: string, body: string}[] = []
+  for (const art of data) {
+    // const {document} = (new JSDOM(await httpRequest.bufferFetch({url: art.link, headers}).then(({data}) => data.toString("utf8")))).window;
+    const docBody = document.querySelector("div[class=\"article-body\"]");
+    if (!docBody) continue;
+    pages.push({
+      title: art.title,
+      body: docBody["innerText"]||docBody.innerHTML
+    });
+  }
+  return pages;
+}
+
+listContent().then(console.log);
